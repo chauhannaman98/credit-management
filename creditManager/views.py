63ecfb94d552ea8de2af749be6e9d05a6f0d394a
Naman Chauhan <chauhannaman98@gmail.com>
2020-07-11 20:39:38 +0530

    add: transfer url

--- creditManager/views.py
+++ creditManager/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def transfer_credit(request):
+    return render(request, 'transfer.html')
