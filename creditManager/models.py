df6ecd9670a8b32d602291895cf7c9cb69120a0a
Naman Chauhan <chauhannaman98@gmail.com>
2020-07-11 20:27:01 +0530

    update: new fields

--- creditManager/models.py
+++ creditManager/models.py
@@ -4,4 +4,10 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=40)
+    pan = models.CharField(max_length=30, default='')
+    phone = models.CharField(max_length=20, default='')
     current_credit = models.IntegerField()
+
+    def __str__(self):
+        return self.name+' | Credit: '+str(self.current_credit)
+    
