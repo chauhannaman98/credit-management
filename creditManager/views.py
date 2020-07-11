5e4db8646b29b5c1ff4954c9c7599dbdac9e119e
Naman Chauhan <chauhannaman98@gmail.com>
2020-07-12 01:24:06 +0530

    update: get context from DB

--- creditManager/views.py
+++ creditManager/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from creditManager.models import User
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def transfer_credit(request):
-    return render(request, 'transfer.html')
+    users = User.objects.all()
+    context = {'users':users}
+    return render(request, 'transfer.html', context)
